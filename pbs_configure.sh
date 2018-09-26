b02c0d936e2e7a6ae3e6bb5e9958ef4851fafa32
victordherbemontagne <victordherbemont@gmail.com>
2018-09-26 12:16:19 +0200

    fix configuration

--- pbs_configure.sh
+++ pbs_configure.sh
@@ -6,7 +6,7 @@ module load anaconda3/5.2
 # Environment config
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda-9.1/lib64
 export CUDA_HOME=/usr/local/cuda-9.1
-conda env create -f environment.yml --force
+conda env create -f config_environment.yml --force
 conda install -c conda-forge nibabel
 conda install tqdm
 
